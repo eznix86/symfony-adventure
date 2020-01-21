72c8e14486e422c58b053eb1e78e229c1b2053a9
Bernard Joseph Jean Bruno <brunobernard1996@outlook.com>
2020-01-21 22:19:55 +0400

    Service container

--- 1. oo/bootstrap.php
+++ 1. oo/bootstrap.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/lib/Ship.php';
 require_once __DIR__.'/lib/BattleManager.php';
 require_once __DIR__.'/lib/ShipLoader.php';
 require_once __DIR__.'/lib/BattleResult.php';
+require_once __DIR__.'/lib/Container.php';
 
 $configuration = array(
     'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
