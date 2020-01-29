36380d01b8448e715146ae8ece33e9ff4a9fff42
Bernard Joseph Jean Bruno <bruno@fizen.fr>
2020-01-29 16:20:55 +0400

    [stripe2] Create Webhook

---
+++ stripe-part-2/src/AppBundle/Controller/WebhookController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Subscription;
+use AppBundle\StripeClient;
+use AppBundle\Subscription\SubscriptionHelper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebhookController extends BaseController
+{
+    /**
+     * @Route("/webhooks/stripe", name="webhook_stripe")
+     *
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function stripeWebhookAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if($data === null)
+        {
+            throw new \Exception("Bad JSON from Stripe");
+        }
+
+        $eventId = $data['id'];
+        /** @var StripeClient $stripeClient */
+        $stripeClient = $this->get('stripe_client');
+
+        $stripeEvent = $stripeClient->findEvent($eventId);
+
+        switch ($stripeEvent->type) {
+            case 'customer.subscription.deleted':
+                $subscriptionId = $stripeEvent->data->object->id;
+                $subscription = $this->findSubscription($subscriptionId);
+
+                /** @var SubscriptionHelper $subscriptionHelper */
+                $subscriptionHelper = $this->get('subscription_helper');
+
+                /** @var Subscription $subscription */
+                $subscriptionHelper->fullyCancelSubscription($subscription);
+
+                break;
+            default:
+                throw new \Exception('Unexpected webhook from stripe: '.$stripeEvent->type);
+        }
+
+        return new Response("Event handled: ".$stripeEvent->type);
+    }
+
+    private function findSubscription($subscriptionId)
+    {
+        $subscription = $this->getDoctrine()->getRepository(SubscriptionHelper::class)
+                    ->findOneBy(
+                        ["stripeSubscriptionId" => $subscriptionId]
+                    );
+
+        if(!$subscription) {
+            throw new \Exception("Somehow we have no subscription id ".$subscriptionId);
+        }
+
+        return $subscription;
+    }
+}
